c1a23ffb593cee277c57e2f30f0b5b4d1243a251
TiagoPedrotkd <tiagopedrosoares02@gmail.com>
2024-12-04 00:59:57 +0000

    update feature selection

--- utils/models.py
+++ utils/models.py
@@ -80,3 +80,30 @@ def monitor_model_rfe(
     logging.info(f"Results saved to {output_csv_path}. Best validation score: {high_score:.4f}")
     print(f"Logs salvos em: {log_file_path}")
     print(f"Resultados salvos em: {output_csv_path}")
+
+
+def select_best_features_and_save(models_data, output_path="best_features_results.csv"):
+    base_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) 
+    results_folder = os.path.join(base_dir, "results")
+    results = []
+    
+    for model_name, df in models_data.items():
+        df['score_diff'] = abs(df['train_score'] - df['val_score'])
+        
+        best_row = df.sort_values(by=['val_score', 'score_diff'], ascending=[False, True]).iloc[0]
+        
+        results.append({
+            "Model": model_name,
+            "Best_nof_features": best_row['nof_features'],
+            "Best_train_score": best_row['train_score'],
+            "Best_val_score": best_row['val_score'],
+            "Score_Diff": best_row['score_diff'],
+            "Selected_features": best_row['selected_features']
+        })
+    
+    results_df = pd.DataFrame(results)
+    
+    output_csv_path = os.path.join(results_folder, output_path)
+
+    results_df.to_csv(output_csv_path, index=False)
+    print(f"Resultados salvos em: {output_csv_path}")
